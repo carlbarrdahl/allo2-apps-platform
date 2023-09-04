83085e6616b3a02647c4afff851ac3701ddda830
Carl Barrdahl <carlbarrdahl@gmail.com>
2023-09-04 16:26:41 +0800

    allo sdk

---
+++ packages/allo-v2-sdk/src/Client/Client.ts
@@ -0,0 +1,14 @@
+import { Chain, PublicClient, Transport, createPublicClient, http } from "viem";
+import { SupportedChain } from "./chains";
+
+export const create = (
+  chain: Chain,
+  rpc?: string
+): PublicClient<Transport, Chain> => {
+  const client = createPublicClient({
+    chain: chain,
+    transport: http(rpc),
+  });
+
+  return client;
+};
---
+++ packages/allo-v2-sdk/src/index.ts
@@ -0,0 +1,3 @@
+export * from "./Allo/Allo";
+export * from "./Registry/Registry";
+export * from "./Client/chains";
